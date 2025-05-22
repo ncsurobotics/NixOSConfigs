ecac9ff7e9d2bb83fc7467f1e3bd1cf1d5c68419
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2025-05-22 20:18:01 +0000

    feat: add generated config

--- hosts/SeaWolf9/default.nix
+++ hosts/SeaWolf9/default.nix
@@ -1,12 +1,22 @@
-_: {
+{pkgs, ...}: {
   imports = [
     ./hardware-configuration.nix
     ./disk-config.nix
   ];
 
   networking.hostName = "SeaWolf9";
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+  
+  environment.systemPackages = with pkgs; [
+    helix
+    vim
+    git
+  ];
 
   time.timeZone = "America/New_York";
 
-  # system.stateVersion = "23.11";
+  system.stateVersion = "24.11";
 }
