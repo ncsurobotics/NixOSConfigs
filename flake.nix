4aa1bdc49476843cb2498899491c2e0097c5beec
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2025-08-23 01:56:55 -0400

    fix: add home manager input

--- flake.nix
+++ flake.nix
@@ -20,6 +20,11 @@
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs =
     inputs:
