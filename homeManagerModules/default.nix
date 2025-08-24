7049cd42acb250c49bc7c80ba186a6290178c4f5
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2025-08-23 23:31:55 -0400

    feat: add yazi

--- homeManagerModules/default.nix
+++ homeManagerModules/default.nix
@@ -8,6 +8,18 @@ _: {
     btop.enable = true;
     ripgrep.enable = true;
     ripgrep-all.enable = true;
+    yazi = {
+      enable = true;
+      shellWrapperName = "y";
+      settings = {
+        mgr = {
+          show_hidden = true;
+          sort_dir_first = true;
+          sort_by = "mtime";
+          sort_reverse = true;
+        };
+      };
+    };
   };
 
   home.stateVersion = "25.05";
